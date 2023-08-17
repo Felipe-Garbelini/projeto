d3edb8f6c461a0dbb5b0cf2ce15cc4338c52833f
Helen <helenmiojodefrango@gmail.com>
2023-08-17 10:54:46 -0300

    mudança avatar

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   // substituir a imagem
-
+  // 
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
     img.setAttribute("src", "./assets/Avatar.png")
